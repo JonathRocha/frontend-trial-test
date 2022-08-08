cd83ea184242aa71dada4456253d53bbce2e01c7
Jonathan Rocha <johnferreirar@gmail.com>
2022-08-08 20:57:02 -0300

    feat: add apollo client

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,6 +1,16 @@
-import { createRoot } from "react-dom/client";
 import { App } from "@/App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { createRoot } from "react-dom/client";
+
+const client = new ApolloClient({
+  uri: process.env.API_URL,
+  cache: new InMemoryCache({ addTypename: false }),
+});
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
