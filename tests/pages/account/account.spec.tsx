1d3ba0a883529cc64d100b47bdd6c1b6436b8194
Jonathan Rocha <johnferreirar@gmail.com>
2022-08-10 12:15:55 -0300

    ensure: account page renders with localized texts

--- tests/pages/account/account.spec.tsx
+++ tests/pages/account/account.spec.tsx
@@ -1,5 +1,6 @@
 import * as loginHooks from "@/hooks/login";
 import * as userHooks from "@/hooks/user";
+import { User } from "@/hooks/user/definition";
 import { Account } from "@/pages/account";
 import { strings } from "@/pages/account/strings";
 import { render, screen } from "@testing-library/react";
@@ -10,12 +11,18 @@ const useUserIdFromTokenStub = jest.spyOn(loginHooks, "useUserIdFromToken");
 const useGetUserQueryStub = jest.spyOn(userHooks, "useGetUserQuery");
 
 describe("Page Account", () => {
+  const user: User = {
+    id: "1",
+    firstName: "John",
+    lastName: "Doe",
+  };
+
   beforeEach(() => {
     useContextStub.mockImplementation(() => ({ language: "EN" }));
     useUserIdFromTokenStub.mockImplementation(() => 1);
     useGetUserQueryStub.mockImplementation(() => ({
       loading: false,
-      data: { user: { id: "1", firstName: "John", lastName: "Doe" } },
+      data: { user },
       error: null,
       client: null,
       called: false,
@@ -35,7 +42,22 @@ describe("Page Account", () => {
   it("Should render without crashing", () => {
     render(<Account />);
     const texts = strings["EN"];
-    const formTitle = screen.getByText(texts.title);
-    expect(formTitle).toBeInTheDocument();
+    const title = screen.getByText(texts.title);
+    expect(title).toBeInTheDocument();
+  });
+
+  it("Should contain texts with localization", () => {
+    useContextStub.mockReturnValue({ language: "PT" });
+
+    render(<Account />);
+
+    const texts = strings["PT"];
+    const title = screen.getByText(texts.title);
+    const firstName = screen.getByLabelText(texts.inputs.firstName.label);
+    const lastName = screen.getByLabelText(texts.inputs.lastName.label);
+
+    expect(title).toBeInTheDocument();
+    expect(firstName).toBeInTheDocument();
+    expect(lastName).toBeInTheDocument();
   });
 });
